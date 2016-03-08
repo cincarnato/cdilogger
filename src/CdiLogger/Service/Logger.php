0bd8bd08c77b257a421a189588255c368eef026f
cristian <cristian@CINCARNATO>
2016-03-08 15:03:12 -0300

    Commit

--- src/CdiLogger/Service/Logger.php
+++ src/CdiLogger/Service/Logger.php
@@ -1,26 +1,37 @@
 <?php
+
 namespace CdiLogger\Service;
+
 use Zend\Log\Logger as ZendLogger;
 use Zend\Authentication\AuthenticationService;
 use Zend\Http\PhpEnvironment\Request;
+
 /**
  * Class Log
  * @package CdiLogger\Service\Log
  */
-class Logger extends ZendLogger
-{
+class Logger extends ZendLogger {
+
     /**
      * @var AuthenticationService
      */
     private $authenticationService;
+
     /**
      * @var Request
      */
     private $request;
+
     /**
      * @var array
      */
     private $customExtra = array();
+
+    /**
+     * @var array
+     */
+    private $optionalExtra = true;
+
     /**
      * @param int   $priority
      * @param mixed $message
@@ -28,76 +39,85 @@ class Logger extends ZendLogger
      *
      * @return ZendLogger
      */
-    final public function log($priority, $message, $extra = array())
-    {
-        $customExtra = array(
-            'Zf2Logger' => array(
+    final public function log($priority, $message, $extra = array()) {
+        $customExtra = array();
+        if ($this->optionalExtra) {
+            $customExtra = array(
                 'sessionId' => session_id(),
-                'host'      => !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'CLI',
-                'ip'        => !empty($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'unavailable'
-            )
-        );
+                'host' => !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'CLI',
+                'ip' => !empty($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'unavailable'
+            );
+        }
         return parent::log($priority, $message, array_merge($extra, $customExtra, $this->customExtra));
     }
+
     /**
      * @param AuthenticationService $authenticationService
      *
      * @return Logger
      */
-    public function setAuthenticationService(AuthenticationService $authenticationService)
-    {
+    public function setAuthenticationService(AuthenticationService $authenticationService) {
         $this->authenticationService = $authenticationService;
         return $this;
     }
+
     /**
      * @return AuthenticationService
      */
-    public function getAuthenticationService()
-    {
+    public function getAuthenticationService() {
         return $this->authenticationService;
     }
+
     /**
      * @param Request $request
      *
      * @return Logger
      */
-    public function setRequest(Request $request)
-    {
+    public function setRequest(Request $request) {
         $this->request = $request;
         return $this;
     }
+
     /**
      * @return Request Request
      */
-    public function getRequest()
-    {
+    public function getRequest() {
         return $this->request;
     }
+
     /**
      * @param array $customExtra
      *
      * @return Logger
      */
-    public function setCustomExtra(array $customExtra)
-    {
+    public function setCustomExtra(array $customExtra) {
         $this->customExtra = $customExtra;
         return $this;
     }
+
     /**
      * @param array $customExtra
      *
      * @return Logger
      */
-    public function addCustomExtra(array $customExtra)
-    {
+    public function addCustomExtra(array $customExtra) {
         $this->customExtra[] = $customExtra;
         return $this;
     }
+
     /**
      * @return array
      */
-    public function getCustomExtra()
-    {
+    public function getCustomExtra() {
         return $this->customExtra;
     }
+
+    function getOptionalExtra() {
+        return $this->optionalExtra;
+    }
+
+    function setOptionalExtra($optionalExtra) {
+        $this->optionalExtra = $optionalExtra;
+    }
+
 }
